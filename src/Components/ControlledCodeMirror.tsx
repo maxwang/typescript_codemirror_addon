0573e934b92ffb2fd1228969ba2548024d3cbb99
Max Wang <max.wang@ebms.com.au>
2020-08-22 13:28:20 +1000

    init

---
+++ src/Components/ControlledCodeMirror.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import "codemirror/mode/xml/xml";
+import 'codemirror/theme/material.css';
+import { FullScreenAddon } from "./FullScreenAddon";
+
+// do not know where is the best place to add addon to system
+FullScreenAddon();
+
+const ControlledCodeMirror: React.FC = () => {
+    const [value, setValue] = useState("this is a test");
+
+    return (
+        <>
+            <CodeMirror
+                value={value}
+                options={{
+                    mode: "xml",
+                    theme: "material",
+                    lineNumbers: true,
+                    lineWrapping: true,
+                    indentWithTabs: true,
+                    gutter: true,
+                    tabSize: 4,
+                    autofocus: true,
+                }}
+                onBeforeChange={(editor, data, value) => {
+                    setValue(value);
+                }}
+                onKeyDown={(editor, event) => {
+                    if (event.code === "F11") {
+                        editor.setOption(
+                            "fullScreen",
+                            !editor.getOption("fullScreen")
+                        );
+
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    if (event.code === "Escape") {
+                        if (editor.getOption("fullScreen")) {
+                            editor.setOption("fullScreen", false);
+                        }
+                    }
+                }}
+            />
+        </>
+    );
+};
+
+export default ControlledCodeMirror;
---
+++ src/Components/FullScreenAddon.tsx
@@ -0,0 +1,47 @@
+import CodeMirror, { Editor } from "codemirror";
+import "./FullScreenAddon.css";
+
+export const FullScreenAddon = (): void => {
+    CodeMirror.defineOption(
+        "fullScreen",
+        false,
+        (editor: Editor, val: boolean, old: boolean) => {
+            if (old === CodeMirror.Init) old = false;
+            if (!old === !val) return;
+            if (val) {
+                setFullscreen(editor);
+            } else {
+                setNormal(editor);
+            }
+        }
+    );
+
+    const setFullscreen = (editor: Editor) => {
+        const wrap = editor.getWrapperElement();
+        editor.state.fullScreenRestore = {
+            scrollTop: window.pageYOffset,
+            scrollLeft: window.pageXOffset,
+            width: wrap.style.width,
+            height: wrap.style.height,
+        };
+        wrap.style.width = "";
+        wrap.style.height = "auto";
+        wrap.className += " CodeMirror-fullscreen";
+        document.documentElement.style.overflow = "hidden";
+        editor.refresh();
+    };
+
+    const setNormal = (editor: Editor) => {
+        const wrap = editor.getWrapperElement();
+        wrap.className = wrap.className.replace(
+            /\s*CodeMirror-fullscreen\b/,
+            ""
+        );
+        document.documentElement.style.overflow = "";
+        const info = editor.state.fullScreenRestore;
+        wrap.style.width = info.width;
+        wrap.style.height = info.height;
+        window.scrollTo(info.scrollLeft, info.scrollTop);
+        editor.refresh();
+    };
+};
